0b00fad1eb3f3bc2f1340631efcb36f99e63fbea
AbdulHafidh-AI <abdul_h20@mhs.unsyiah.ac.id>
2023-03-26 23:00:42 +0700

    refactor: changing register part

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,49 +8,73 @@
 @section('content')
     <!-- Register form that includes the name, npm, password  using tailwind css -->
 
-    <div class="w-full max-w-sm mx-auto">
-
-        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-
-            <form  action="/register" method="POST">
-                @csrf <!-- Cross Site Request Forgery -->
-                <div class="mb-4">
-                    <label class="block text-gray-700 font-bold mb-2" for="name">
-                        Nama
-                    </label>
-                    <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="name" name="name" type="text" placeholder="John Doe">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700 font-bold mb-2" for="npm">
-                        NPM
-                    </label>
-                    <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="npm" name="npm" type="text" placeholder="2008107010056">
-                </div>
-                <div class="mb-6">
-                    <label class="block text-gray-700 font-bold mb-2" for="password">
-                        Password
-                    </label>
-                    <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="password" name="password" type="password" placeholder="********">
-                </div>
-                <div class="flex items-center justify-between ">
-                    <button type="submit"
-                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                       >
-                        Register
-                    </button>
-                </div>
-            </form>
 
 
-        </div>
-        
-    </div>
 
 
-@endsection
+        <div class="min-h-screen flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div class="sm:mx-auto sm:w-full sm:max-w-md">
+                <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                    Silahkan Register
+                </h2>
+            </div>
+
+            <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <form action="/register" method="POST">
+                        @csrf
+
+                        <!-- Name -->
+                        <div>
+                            <label for="name" class="block text-sm font-medium text-gray-700">
+                                Name
+                            </label>
+                            <div class="mt-1">
+                                <input id="name" name="name" required
+                                    class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                                    placeholder="Silahkan masukkan Nama anda" required>
+                            </div>
+
+                        <!-- NPM -->
+                        <div>
+                            <label for="NPM" class="block text-sm font-medium text-gray-700">
+                                NPM
+                            </label>
+                            <div class="mt-1">
+                                <input id="npm" name="npm" required
+                                    class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                                    placeholder="Silahkan masukkan NPM anda" required>
+                            </div>
+                        </div>
+
+                        <div>
+                            <label for="password" class="block text-sm font-medium text-gray-700">
+                                Password
+                            </label>
+                            <div class="mt-1">
+                                <input id="password" name="password" type="password" autocomplete="current-password"
+                                    required
+                                    class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                                    placeholder="Masukkan Password Anda" required>
+                            </div>
+                        </div>
+
+                        <!-- make button for login -->
+                        <div>
+                            <button type="submit"
+                                class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                                    <!-- Heroicon name: solid/lock-closed -->
+                                    <svg class="h-5 w-5 text-blue-500 group-hover:text-blue-400"
+                                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                        aria-hidden="true">
+                                        <path fill-rule="evenodd"
+                                            d="M5 8a3 3 0 016 0v1h1a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2h1V8zm3 0V7a1 1 0 10-2 0v1H5a1 1 0 00-1 1v5a1 1 0 001 1h10a1 1 0 001-1v-5a1 1 0 00-1-1h-4z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </span>
+                                Register
+                            </button>
+                        </div>
+                    </form>
+                @endsection
